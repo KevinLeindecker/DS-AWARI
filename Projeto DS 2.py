49408f8e58caedf266d197f652d065aae0c94a9d
Kevin Leindecker <102391777+KevinLeindecker@users.noreply.github.com>
2022-08-01 13:55:42 -0300

    Add files via upload
    Avaliação de Séries Temporais

---
+++ Projeto DS 2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 21:16:42 2022
+
+@author: User
+"""
+
+#importando as bibliotecas
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels as sm
+from pprint import pprint
+from pycaret.regression import *
+from sktime.utils.plotting import plot_series
